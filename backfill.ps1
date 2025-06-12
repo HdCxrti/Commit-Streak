d28d6df7032da570977631726ccd30a133e36da1
HdCxrti <124410988+HdCxrti@users.noreply.github.com>
2025-06-12 10:32:55 -0400

    Add backfill script for automated commit creation

---
+++ backfill.ps1
@@ -0,0 +1,10 @@
+$dates = @("2024-06-10", "2024-06-11")
+
+foreach ($date in $dates) {
+    $env:GIT_AUTHOR_DATE = "$date 12:00:00"
+    $env:GIT_COMMITTER_DATE = "$date 12:00:00"
+    
+    git commit --allow-empty -m "Backfilled commit for $date"
+}
+
+git push origin main
